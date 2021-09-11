d9b37cc1e487e00c7ff7c49289512a6fb4d69ec4
atefeh-k <kazemi.att@gmail.com>
2021-09-11 21:54:52 +0430

    commit test

---
+++ src/app/core/service/storage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StorageService {
+    public clear(): void {
+        sessionStorage.clear();
+    }
+    public store(key: string, value: string){
+        sessionStorage.setItem(key,value);
+    }
+
+    public load(key:string): string{
+        return sessionStorage.getItem(key);
+    }
+
+}
